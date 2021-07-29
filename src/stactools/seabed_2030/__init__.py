c0e7f1d373d674836a01ef3074594b9dd2df0129
Tyler Battle <tyler@battle.ca>
2021-07-29 15:56:06 -0700

    Make `mypy` more strict. Add type annotations.

--- src/stactools/seabed_2030/__init__.py
+++ src/stactools/seabed_2030/__init__.py
@@ -1,4 +1,6 @@
 import stactools.core
+from stactools.cli import Registry
+from stactools.seabed_2030 import commands
 from stactools.seabed_2030.stac import create_collection, create_item
 
 __all__ = ['create_collection', 'create_item']
@@ -6,8 +8,7 @@ __all__ = ['create_collection', 'create_item']
 stactools.core.use_fsspec()
 
 
-def register_plugin(registry):
-    from stactools.seabed_2030 import commands
+def register_plugin(registry: Registry) -> None:
     registry.register_subcommand(commands.create_seabed2030_command)
 
 
